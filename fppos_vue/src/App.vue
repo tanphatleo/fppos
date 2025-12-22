141cd0eff99f3d831e9916e5d3ca866f5e5c26fa
Phat <tanphat.leo@gmail.com>
2025-12-23 00:24:51 +0700

    working

--- fppos_vue/src/App.vue
+++ fppos_vue/src/App.vue
@@ -1,12 +1,13 @@
 <template>
-  <nav>
+  <!-- <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  </nav> -->
   <router-view/>
 </template>
 
 <style lang="scss">
+@import '../node_modules/bulma/';
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- fppos_vue/src/router/index.js
+++ fppos_vue/src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Sale from '@/views/Sale.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/sale',
+    name: 'sale',
+    component: Sale
   },
   {
     path: '/about',
