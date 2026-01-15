9a2352c6e5491a033ce2e6e73a630387725b99cd
Phat <tanphat.leo@gmail.com>
2026-01-15 21:21:32 +0700

    working

--- fppos/fppos/settingsProdSG.py
+++ fppos/fppos/settingsProdSG.py
@@ -16,6 +16,7 @@ from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
